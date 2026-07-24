044aeefdcef77b9a9115a465deb558aa2f99539f
legion <2661580914a@gmail.com>
2026-07-24 20:48:43 +0800

    chore: 完善模块依赖关系

--- app/src/main/java/com/codesight/app/CodeSightApplication.java
+++ app/src/main/java/com/codesight/app/CodeSightApplication.java
@@ -3,7 +3,10 @@ package com.codesight.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.ComponentScan;
+
 @SpringBootApplication
+@ComponentScan("com.codesight")
 public class CodeSightApplication {
 
 	public static void main(String[] args) {
