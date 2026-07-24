1dda41b436fda8f322af8ef5354d2a4650ab8e09
legion <2661580914a@gmail.com>
2026-07-24 18:59:54 +0800

    feat: 添加Knife4j依赖和用户实体类的OpenAPI注解

---
+++ app/src/main/java/com/codesight/app/config/Knife4jConfig.java
@@ -0,0 +1,21 @@
+package com.codesight.app.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Knife4jConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("CodeSight 接口文档")
+                        .version("1.0")
+                        .description("CodeSight 后端 API")
+                        .contact(new Contact().name("CodeSight")));
+    }
+}
