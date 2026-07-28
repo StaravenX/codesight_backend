c577ac0f6d0ce14438aa58c189ceaba00d6c5763
legion <2661580914a@gmail.com>
2026-07-28 22:58:10 +0800

    feat: 新增客户端信息模型与标识校验工具

--- auth/src/main/java/com/codesight/auth/config/AuthConfig.java
+++ auth/src/main/java/com/codesight/auth/config/AuthConfig.java
@@ -1,5 +1,6 @@
 package com.codesight.auth.config;
 
+import com.codesight.auth.utils.PemUtils;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
