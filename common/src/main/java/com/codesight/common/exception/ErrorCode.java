1be0e787492af781228d36dc4d4975ef8093dbac
legion <2661580914a@gmail.com>
2026-08-01 19:36:06 +0800

    fix: 修复验证码倒计时刷新导致封锁机制失效的bug

--- common/src/main/java/com/codesight/common/exception/ErrorCode.java
+++ common/src/main/java/com/codesight/common/exception/ErrorCode.java
@@ -7,7 +7,8 @@ public enum ErrorCode {
 
     BAD_REQUEST("BAD_REQUEST", "业务参数错误"),
     VERIFICATION_RATE_LIMIT("VERIFICATION_RATE_LIMIT", "验证码发送过于频繁"),
-    VERIFICATION_DAILY_LIMIT("VERIFICATION_DAILY_LIMIT", "验证码发送次数达到上限");
+    VERIFICATION_DAILY_LIMIT("VERIFICATION_DAILY_LIMIT", "验证码发送次数达到上限"),
+    VERIFICATION_LOCKED("VERIFICATION_LOCKED", "验证尝试次数过多，账户已被锁定");
 
     final String code;
     final String msg;
