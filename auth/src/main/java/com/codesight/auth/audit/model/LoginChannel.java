678847a36955fe6ce566e115309bf772917f4cd9
legion <2661580914a@gmail.com>
2026-08-10 23:58:00 +0800

    feat(auth): 添加密码重置功能

--- auth/src/main/java/com/codesight/auth/audit/model/LoginChannel.java
+++ auth/src/main/java/com/codesight/auth/audit/model/LoginChannel.java
@@ -10,7 +10,8 @@ public enum LoginChannel {
     REGISTER("REGISTER", "账号注册"),
     PASSWORD("PASSWORD", "密码登录"),
     CODE("CODE", "验证码登录"),
-    TOKEN_REFRESH("TOKEN_REFRESH", "令牌刷新");
+    TOKEN_REFRESH("TOKEN_REFRESH", "令牌刷新"),
+    PASSWORD_RESET("PASSWORD_RESET", "密码重置");
 
     @EnumValue
     private final String code;
