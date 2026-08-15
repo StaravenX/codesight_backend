94770f46a9c18e3e2b13f45873c6c3c7f5a01519
legion <2661580914a@gmail.com>
2026-08-15 23:24:09 +0800

    build(project): 引入 dependencyManagement 统一版本管理并重构各子模块依赖边界

--- app/src/main/java/com/codesight/app/config/MpConfig.java
+++ app/src/main/java/com/codesight/app/config/MpConfig.java
@@ -1,4 +1,4 @@
-package com.codesight.common.config;
+package com.codesight.app.config;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
